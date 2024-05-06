f4388d1bade10364a48bf86b3d68074d3100904f
Uhgtred <Markus_koesters@gmx.de>
2024-05-06 22:17:27 +0200

    structure of the output is now correct
    the structure of the raw-sensor data is now correctly formatted. Next the code needs to be cleaned up and tested. The Soilmoisture-sensors are already working though the soilmoisture-sensor itself needs to be calibrated on the receiving side. But the new sht31 is not yet funcitonal and dht has been too unprecise in the past.

--- sensorBoard/sensorBoard.ino
+++ sensorBoard/sensorBoard.ino
@@ -1,14 +1,21 @@
 #include <Grove_Temperature_And_Humidity_Sensor.h>
 #include <ArduinoJson.h>
+#include <Arduino.h>
+#include <Wire.h>
+#include "Adafruit_SHT31.h"
 
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
-#define DHTPIN A0     // what pin we're connected to
-DHT dht(DHTPIN, DHTTYPE);
+Adafruit_SHT31 sht31 = Adafruit_SHT31();
+// #define DHTTYPE DHT22   // DHT 22  (AM2302)
+// #define DHTPIN A0     // what pin we're connected to
+// DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
     Serial.begin(115200);
-    Wire.begin();
-    dht.begin();
+    // initializing i2c-connection with sht31 sensor
+    if (! sht31.begin(0x44)) {   
+      Serial.println("Couldn't find SHT31");
+    }
+    // dht.begin();
 }
 
 const int tempHumiditySensorSize = 1;
@@ -28,7 +35,7 @@ void loop() {
     float soilMoistureValues[soilMoistureSize];
 
     iterateTempHumiditySensors(tempHumidityValues, tempHumiditySensors, tempHumiditySensorSize);
-    addJsonArray(documentPointer, "tempHumidity", tempHumidityValues, tempHumiditySensorSize);
+    addJsonTempHumidityValue(documentPointer, "tempHumidity", *tempHumidityValues, tempHumiditySensorSize);
     iterateSoilMoistureSensors(soilMoistureValues, soilMoistureSensors, soilMoistureSize);
     addJsonArray(documentPointer, "soilMoisture", soilMoistureValues, soilMoistureSize);
 
@@ -58,7 +65,7 @@ void iterateTempHumiditySensors(struct tempHumidityData *dataArray, int *pinArra
     */
     for (int pinCounter=0; pinCounter<arraySize; pinCounter++){
         struct tempHumidityData data;
-        data = readDHT22(pinArray[pinCounter]);
+        data = readSHT31(pinArray[pinCounter]);
         dataArray[pinCounter] = data;
     }
 }
@@ -80,24 +87,55 @@ void addJsonArray(DynamicJsonDocument *jsonDoc, String name, float *sensorValues
     :param sensorValues: Array of sensor-values that is being stored as value to the specified key.
     :param arraySize: Number of values that are going to be stored inside the json-document.
     */
-    JsonArray jsonArray = jsonDoc->createNestedArray(name);
+    DynamicJsonDocument innerDocument(50);
+    JsonArray innerArray = innerDocument.createNestedArray(name);
+    JsonObject soilMoistureObject = innerDocument.createNestedObject();
     for(int counter = 0; counter < arraySize; counter++){
-        jsonArray.add(sensorValues[counter]);
+        innerArray.add(sensorValues[counter]);
     }
+    jsonDoc->add(innerDocument);
 }
 
-tempHumidityData readDHT22(){
+void addJsonTempHumidityValue(DynamicJsonDocument *jsonDoc, String name, struct tempHumidityData sensorStruct , int arraySize){
     /*
-    Method for reading a DHT22-sensor (Not recommended! That sensor-precision is shit!).
-    :return: The raw-values <int> read from the sensor.
+    Method for adding an array to a json-document.
+    :param jsonDoc: json-document, that the array will be stored in.
+    :param name: key of the array inside the json-document.
+    :param sensorValues: Array of sensor-values that is being stored as value to the specified key.
+    :param arraySize: Number of values that are going to be stored inside the json-document.
+    */
+    DynamicJsonDocument innerDocument(50);
+    JsonArray baseArray = innerDocument.createNestedArray(name);
+    JsonObject jsonObject = baseArray.createNestedObject();
+    jsonObject["temperature"] = sensorStruct.temp;
+    jsonObject["humidity"] = sensorStruct.humidity;
+    jsonDoc->add(innerDocument);
+}
+
+tempHumidityData readSHT31(int pin){
+    /*
+    Method for reading a SHT31-sensor
+    :param pin: unused for the moment. Maybe there will be the possibility to read from multiple sensors later.
+    :return: The raw values <tempHumidityData> read from the sensor.
     */
     struct tempHumidityData sensorData;
-    float sensorValueArray[2];
-    dht.readTempAndHumidity(sensorValueArray);
-    sensorData.temp = sensorValueArray[0];
-    sensorData.humidity = sensorValueArray[1];
+    sensorData.temp = sht31.readTemperature();
+    sensorData.humidity = sht31.readHumidity();
     return sensorData;
 }
+
+// tempHumidityData readDHT22(){
+//     /*
+//     Method for reading a DHT22-sensor (Not recommended! That sensor-precision is shit!).
+//     :return: The raw-values <int> read from the sensor.
+//     */
+//     struct tempHumidityData sensorData;
+//     float sensorValueArray[2];
+//     dht.readTempAndHumidity(sensorValueArray);
+//     sensorData.temp = sensorValueArray[0];
+//     sensorData.humidity = sensorValueArray[1];
+//     return sensorData;
+// }
 
 int readSoilMoisture(int pin){
     /*
