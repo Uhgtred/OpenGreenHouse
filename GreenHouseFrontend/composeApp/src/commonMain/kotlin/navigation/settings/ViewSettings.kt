5a244c58ca4269b862cce579a3ccd84bd1238a66
Uhgtred <Markus_koesters@gmx.de>
2024-07-30 22:28:19 +0200

    Update ViewSettings.kt

--- GreenHouseFrontend/composeApp/src/commonMain/kotlin/navigation/settings/ViewSettings.kt
+++ GreenHouseFrontend/composeApp/src/commonMain/kotlin/navigation/settings/ViewSettings.kt
@@ -1,21 +1,14 @@
 package navigation.settings
 
-import Data.SharedSettings
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Switch
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.core.screen.Screen
-import androidx.compose.ui.platform.LocalContext
+
+//import androidx.compose.ui.platform.LocalContext
 
 class ViewSettingsScreen: Screen {
 
@@ -26,31 +19,31 @@ class ViewSettingsScreen: Screen {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ){
-            DarkModeToggleSwitch()
+//            DarkModeToggleSwitch()
         }
     }
 
 }
 
-@Composable
-fun DarkModeToggleSwitch() {
-    SharedSettings: SharedSettings,
-    ) {
-        val context = LocalContext.current
-        var isDarkMode by remember { mutableStateOf(false) }
-
-        LaunchedEffect(key1 = Unit) {
-            settingsRepository.observeDarkMode().collect { darkMode ->
-                isDarkMode = darkMode
-            }
-        }
-
-        Switch(
-            checked = isDarkMode,
-            onCheckedChange = { newMode ->
-                isDarkMode = newMode
-                settingsRepository.toggleDarkMode(newMode)
-            }
-        )
-        Text(text = if (isDarkMode) "Dark Mode" else "Light Mode")
-}
+//@Composable
+//fun DarkModeToggleSwitch() {
+//    SharedSettings: SharedSettings,
+//    ) {
+//        val context = LocalContext.current
+//        var isDarkMode by remember { mutableStateOf(false) }
+//
+//        LaunchedEffect(key1 = Unit) {
+//            settingsRepository.observeDarkMode().collect { darkMode ->
+//                isDarkMode = darkMode
+//            }
+//        }
+//
+//        Switch(
+//            checked = isDarkMode,
+//            onCheckedChange = { newMode ->
+//                isDarkMode = newMode
+//                settingsRepository.toggleDarkMode(newMode)
+//            }
+//        )
+//        Text(text = if (isDarkMode) "Dark Mode" else "Light Mode")
+//}
