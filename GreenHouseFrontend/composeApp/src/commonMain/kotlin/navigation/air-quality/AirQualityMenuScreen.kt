7606bed21679da6fde89b86c9b00cc34dc72ba9a
Uhgtred <Markus_koesters@gmx.de>
2024-08-09 13:46:26 +0200

    added a back-button (arrow) to the air-quality page

--- GreenHouseFrontend/composeApp/src/commonMain/kotlin/navigation/air-quality/AirQualityMenuScreen.kt
+++ GreenHouseFrontend/composeApp/src/commonMain/kotlin/navigation/air-quality/AirQualityMenuScreen.kt
@@ -6,10 +6,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Air
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -26,16 +29,31 @@ class AirQualityMenuScreen: Screen {
 
     @Composable
     override fun Content(){
+        TopAppBarWithBackButton("Air-Quality")
         Column(
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ){
             val relativeHumidity = 0.0
-            Text("Air-Quality")
+//            Text("Air-Quality")
             Text("Relative Humidity: ${relativeHumidity}%")
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBarWithBackButton(title: String) {
+    val navigator = LocalNavigator.currentOrThrow
+    TopAppBar(
+        title = { Text(title) },
+        navigationIcon = {
+            IconButton(onClick = { navigator.popUntilRoot() }) {
+                Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
+            }
+        }
+    )
 }
 
 @Composable
